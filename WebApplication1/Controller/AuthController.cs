7b61368dd74a7bf8e52c53cc10eb6c9513efd1d0
ArsenyVtorushin <vtorusinarsenij60@gmail.com>
2026-03-31 18:51:40 +0500

    Add project files.

---
+++ WebApplication1/Controller/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Service;
+
+
+namespace WebApplication1.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")] // https://localhost:7190/api/auth
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+
+
+        //REST API
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login([FromBody] LoginRequest request)
+        {
+            try
+            {
+                var response = await _authService.Authenticate(request);
+                if (response == null)
+                    return Unauthorized("Invalid token or password");
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost("register")] // https://localhost:7190/api/auth/register (POST)
+        public async Task<ActionResult<string>> Register([FromBody] LoginRequest request)
+        {
+            try
+            {
+                var response = await _authService.Register(request);
+                if (response == null)
+                    return Unauthorized("Invalid token or password");
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ WebApplication1/Service/AuthService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+
+namespace WebApplication1.Service
+{
+    public interface IAuthService
+    {
+        Task<string?> Authenticate(LoginRequest request);
+        Task<bool> Register(LoginRequest request);
+    }
+    public class AuthService : IAuthService
+    {
+        // Авторизация - уста
+        public async Task<string?> Authenticate(LoginRequest request)
+        {
+            return "";
+        }
+
+        public async Task<bool> Register(LoginRequest request)
+        {
+            return true;
+        }
+    }
+}
